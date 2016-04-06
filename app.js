6338b66aef61f839bb1a3fa87141e0068789eb3a
zhengc <victoria0217@gmail.com>
2016-04-05 18:59:17 -0700

    try post

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ app.get("/", function(req, res) {
  res.send("Heroku Demo!");
 });
 
+app.post("/fogbugz", function(req, res) {
+  console.log("Command received");
+  var text = req.body.text
+  res.send(text)
+});
+
 // bind the app to listen for connections on a specified port
 var port = process.env.PORT || 3000;
 app.listen(port);
